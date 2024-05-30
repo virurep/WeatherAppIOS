e9a095e1de5afb356bb369deca0b8f785599522d
virurepalle <virurep@uw.edu>
2024-05-29 21:57:59 -0700

    hourly done

---
+++ spline-test/Managers/HourlyForecastManager.swift
@@ -0,0 +1,8 @@
+//
+//  HourlyForecastManager.swift
+//  spline-test
+//
+//  Created by Viru Repalle on 5/29/24.
+//
+
+import Foundation
---
+++ spline-test/Views/HourlyView.swift
@@ -0,0 +1,18 @@
+//
+//  HourlyView.swift
+//  spline-test
+//
+//  Created by Viru Repalle on 5/29/24.
+//
+
+import SwiftUI
+
+struct HourlyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HourlyView()
+}
